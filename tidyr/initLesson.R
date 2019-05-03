34122f9373b93d7673f56a6910fec28ef5cf20c9
matt-dray <mwdray@gmail.com>
2019-05-03 07:43:10 +0100

    Expand the missing values section

--- tidyr/initLesson.R
+++ tidyr/initLesson.R
@@ -2,3 +2,8 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
+
+table_na <- data.frame(
+  x1 = LETTERS[1:5],
+  x2 = c(1, NA, NA, 3, NA)
+)
