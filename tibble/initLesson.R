dabcdcfab8fd2b0877870ec4cbcf3543989bb9a5
matt-dray <mwdray@gmail.com>
2019-10-30 08:51:24 +0000

    Correct typos, rephrase sentences, add leg_count tibble, add URL questions

--- tibble/initLesson.R
+++ tibble/initLesson.R
@@ -3,4 +3,13 @@
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
 
+# Named vector to use in neframe() example
 legs_vec <- c("cat" = 4, "snake"= 0, "bird" = 2)
+
+# Tibble to practice usine add_row() and add_column()
+leg_count <- tibble::tribble(
+  ~animal, ~legs,
+  "cat", 4,
+  "snake", 0,
+  "bird", 2
+)
